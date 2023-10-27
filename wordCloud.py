26d3210f136a14be8a47ea5405e89d869ff84738
Lyainc <koore12@gmail.com>
2023-10-27 18:07:25 +0900

    로고이미지 적용

--- wordCloud.py
+++ wordCloud.py
@@ -1,17 +1,19 @@
 import pandas as pd
+import numpy as np
 from konlpy.tag import Okt
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from datetime import datetime
+from PIL import Image
 
 # Define a list of custom Korean stopwords
-custom_stopwords = ["것", "하는", "되는", "때", "있는", "대한"]
+custom_stopwords = ["것", "하는", "되는", "때", "있는", "대한", "할", "더", "일", "등", "대해", "없이", "수", "및", "하여"]
 
 # Define a list of meaningless morphemes (POS tags to exclude)
 meaningless_pos = ["Josa", "Punctuation", "Eomi", "Suffix", "Conjunction"]
 
 # Define a function to read a CSV file and generate a word cloud
-def generate_korean_wordcloud(csv_file, output_directory):
+def generate_korean_wordcloud(csv_file, output_directory, mask_image_path):
     # Read the CSV file (make sure it contains a column with Korean text)
     data = pd.read_csv(csv_file)
 
@@ -30,8 +32,12 @@ def generate_korean_wordcloud(csv_file, output_directory):
     # Specify the local font path for Nanum Gothic
     font_path = 'data/NanumSquareR.ttf'  # Replace with the actual path to the TTF font file
 
+    # Load the mask image
+    mask_image = np.array(Image.open(mask_image_path))
+
     # Generate the word cloud with the local Nanum Gothic font
     wordcloud = WordCloud(
+        mask=mask_image,
         font_path=font_path,
         background_color='white',
         width=800,
@@ -56,4 +62,5 @@ def generate_korean_wordcloud(csv_file, output_directory):
 # Usage
 csv_file = 'data/조직문화_rawdata - 장점.csv'
 output_directory = 'wordcloud/'  # Replace with the path to the directory where you want to save the file
-generate_korean_wordcloud(csv_file, output_directory)
+mask_image_path = 'data/logo_mask.png'  # Replace with the path to your custom mask image
+generate_korean_wordcloud(csv_file, output_directory, mask_image_path)
